218115afff811e9978498e8e7314dae4eeaadd8f
nyash <alex-nyashin1996@yandex.ru>
2022-11-13 21:20:29 +0300

    add Configuration.remote in BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,13 +1,9 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.WebDriver;
-
-import static com.codeborne.selenide.Selenide.open;
 
 public class BaseTest {
 
@@ -19,17 +15,14 @@ public class BaseTest {
     static void openMainPage() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://www.warframe.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @BeforeEach
-    void generationDate(){
-        email = faker.internet().emailAddress();;
+    void generationDate() {
+        email = faker.internet().emailAddress();
+        ;
         nickname = faker.funnyName().name();
-        password = faker.internet().password(8,25);
+        password = faker.internet().password(8, 25);
     }
-
-//    @BeforeEach
-//    void closeBrowser(){
-//        Selenide.closeWebDriver();
-//    }
 }
