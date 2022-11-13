234ac0281f0af9571e5d1175a253d43de685a86b
nyash <alex-nyashin1996@yandex.ru>
2022-11-13 21:47:49 +0300

    add Attach

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -3,7 +3,9 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import com.github.javafaker.Faker;
+import helper.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -18,22 +20,21 @@ public class BaseTest {
     static void openMainPage() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://www.warframe.com";
-        Configuration.browser = "chrome";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-
-        Configuration.browserCapabilities = capabilities;
     }
 
     @BeforeEach
     void generationDate() {
         email = faker.internet().emailAddress();
-        ;
         nickname = faker.funnyName().name();
         password = faker.internet().password(8, 25);
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 }
