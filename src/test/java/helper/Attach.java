7b600227ea45467f300bfd84507416248c393d33
nyash <alex-nyashin1996@yandex.ru>
2022-12-12 21:09:19 +0300

    add attach video

--- src/test/java/helper/Attach.java
+++ src/test/java/helper/Attach.java
@@ -30,12 +30,18 @@ public class Attach {
     }
 
     public static void browserConsoleLogs() {
-        attachAsText("Browser console logs", String.join("\n", Selenide.getWebDriverLogs(BROWSER)));
+        attachAsText(
+                "Browser console logs",
+                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+        );
     }
+
 
     @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
     public static String addVideo() {
-        return "<html><body><video width='100%' height='100%' controls autoplay><source src='" + getVideoUrl() + "' type='video/mp4'></video></body></html>";
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl()
+                + "' type='video/mp4'></video></body></html>";
     }
 
     public static URL getVideoUrl() {
